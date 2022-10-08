22ccb476355c8f5b458be4c52eab8d9ed209116e
thegestalt <lordweasel6@gmail.com>
2022-10-08 07:25:24 -0600

    put urls back.

--- dreamscape_back/src/app.module.ts
+++ dreamscape_back/src/app.module.ts
@@ -4,8 +4,9 @@ import { DreamsModule } from './dreamscape-dreams/dreams.module';
 import { SessionsModule } from './dreamscape-sessions/sessions.module';
 import { SettingsModule } from './dreamscape-settings/settings.module';
 
-const url = 'mongodb://192.168.1.250';
-// const url = process.env.MONGODB_CONNSTRING;
+// const url = 'mongodb://192.168.1.250';
+const url = process.env.MONGODB_CONNSTRING; // prod
+
 @Module({
   imports: [
     MongooseModule.forRoot(`${url}/dreamscape`, {
