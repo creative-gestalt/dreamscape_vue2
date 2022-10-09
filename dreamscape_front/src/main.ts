eecaac3651f0cc4ece62dc96e0bae44d803fa671
thegestalt <lordweasel6@gmail.com>
2022-10-09 11:23:01 -0600

    added pwa support

--- dreamscape_front/src/main.ts
+++ dreamscape_front/src/main.ts
@@ -4,6 +4,7 @@ import router from "./router";
 import vuetify from "./plugins/vuetify";
 import { pinia } from "./plugins/pinia";
 import { alphaToHex } from "@/utils/constants";
+import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
 document.title = "Dreamscape";
--- dreamscape_front/vue.config.js
+++ dreamscape_front/vue.config.js
@@ -4,4 +4,20 @@ module.exports = {
   devServer: {
     port: 8080,
   },
+  pwa: {
+    name: "Dreamscape",
+    start_url: "index.html",
+    // themeColor: "#4DBA87",
+    // msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+
+    // configure the workbox plugin
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: "dev/sw.js",
+      // ...other Workbox options...
+    },
+  },
 };
